78ad4244f414aa25fc03cbbcbc6203bb9fab854e
Blue <161075136+PacssauroRex@users.noreply.github.com>
2024-09-29 00:12:43 -0300

    Add files via upload

---
+++ Aresta.java
@@ -0,0 +1,11 @@
+package org.yourcompany.yourproject;
+
+public class Aresta {
+    String destino;
+    int peso;
+
+    public Aresta(String origem, String destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+}
